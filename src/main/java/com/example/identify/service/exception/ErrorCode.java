9d5560fc0daf5281c5cbee0d6d8b21fc738d83bf
Senkiro <101233448+Senkiro@users.noreply.github.com>
2024-04-15 12:13:03 +0700

    fix bug role user

--- src/main/java/com/example/identify/service/exception/ErrorCode.java
+++ src/main/java/com/example/identify/service/exception/ErrorCode.java
@@ -1,29 +1,31 @@
 package com.example.identify.service.exception;
 
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+@Getter
 public enum ErrorCode {
-    UNCATEGORIZED_EXCEPTION(9999,"Uncategorized error"),
-    INVALID_KEY(1000,"Invalid message key"),
-    USER_EXISTED(1001,"User existed"),
-    USERNAME_INVALID(1002,"Username must be at least 3 character"),
-    USERPASSWORD_INVALID(1003,"Password must be at least 8 character"),
-    USER_NOT_EXISTED(1004,"User not existed"),
-    UNAUTHENTICATED(1005,"Unauthenticated")
+    UNCATEGORIZED_EXCEPTION(9999,"Uncategorized error", HttpStatus.INTERNAL_SERVER_ERROR),
+    INVALID_KEY(1000,"Invalid message key",HttpStatus.BAD_REQUEST),
+    USER_EXISTED(1001,"User existed",HttpStatus.BAD_REQUEST),
+    USERNAME_INVALID(1002,"Username must be at least 3 character",HttpStatus.BAD_REQUEST),
+    USERPASSWORD_INVALID(1003,"Password must be at least 8 character",HttpStatus.BAD_REQUEST),
+    USER_NOT_EXISTED(1004,"User not existed",HttpStatus.NOT_FOUND),
+    UNAUTHENTICATED(1005,"Unauthenticated",HttpStatus.UNAUTHORIZED),
+    UNAUTHORIZED(1006,"You do not have permission",HttpStatus.FORBIDDEN),
 
     ;
 
-    ErrorCode(int code, String message) {
+    ErrorCode(int code, String message, HttpStatusCode statusCode) {
         this.code = code;
         this.message = message;
+        this.statusCode = statusCode;
     }
 
     private int code;
     private String message;
+    private HttpStatusCode statusCode;
 
-    public int getCode() {
-        return code;
-    }
 
-    public String getMessage() {
-        return message;
-    }
 }
